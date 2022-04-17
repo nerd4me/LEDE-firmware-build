a9c58da6d990f218923ddda39112b785d3a429a4
YongDing Zhou <zhouyongding@zhikan.net.cn>
2022-04-17 19:17:25 +0800

    add r2s support

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
